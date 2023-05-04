f269a838b79abf11effbc0bc29980721213a7baa
Kyubravo <MOoOHMMD24@hotmail.com>
2023-05-04 10:19:05 +0300

    js script minor improvements

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-let id = 0;
-
 addTaskBtn = document.getElementById("addTaskBtn");
 taskList = document.getElementById("taskList");
 addTaskBtn.addEventListener("click", addTask);
 
+let id = 0;
 function addTask() {
     // Remove "No Items" text
     if (id == 0) {
@@ -11,18 +10,19 @@ function addTask() {
     }
 
     // Create List Item
-    newTask = document.createElement('li');
+    let newTask = document.createElement('li');
     taskList.appendChild(newTask);
 
-    div = document.createElement('div');
+    let div = document.createElement('div');
     newTask.appendChild(div);
-    div.innerHTML = `<div class="hstack mt-2 mb-2 ms-4 me-4">
-    <div class="input-group me-4">
-        <span class="input-group-text" id="taskIndex${id}">${id}</span>
-        <input type="text" id="task${id}" class="form-control" placeholder="Task name" aria-label="Task name">
-    </div>
-    <input class="form-check-input" type="checkbox" id="c${id}">
-</div>`;
+    div.innerHTML = `
+    <div class="hstack mt-2 mb-2 ms-4 me-4">
+        <div class="input-group me-4">
+            <span class="input-group-text" id="taskIndex${id}">${id}</span>
+            <input type="text" id="task${id}" class="form-control" placeholder="Task name" aria-label="Task name">
+        </div>
+        <input class="form-check-input" type="checkbox" id="c${id}">
+    </div>`;
     newTask.appendChild(document.createElement('hr'));
 
     // Increment the id
