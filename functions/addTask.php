11218984ed452cf19ad4124805bef13900af9a4f
Kyubravo <MOoOHMMD24@hotmail.com>
2023-06-14 13:53:40 +0300

    removed var_dump()

--- functions/addTask.php
+++ functions/addTask.php
@@ -1,11 +1,6 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["taskName"])){
-
-    echo "<pre>";
-    echo var_dump($_SERVER);
-    echo "</pre>";
-    //die();
     
     $query = "INSERT INTO items (title, is_complete) VALUES (? , 0)";
 
